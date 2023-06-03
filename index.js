2f30339cdd16333a24a9c62c40c0e4e29c4c7685
defineseventy <jacweizhen6679@gmail.com>
2023-06-03 17:01:44 +0800

    Update index.js

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ let playScore = 0;
 let button = document.querySelectorAll(".button");
 const body = document.querySelectorAll("body");
 const main = document.querySelectorAll("main");
+let i = 0;
 let txt = 'Made using Javascript for a TOP project \n Are you brave enough to challenge the Almighty Computer? \n If you dare to take on this challenge.... The first to reach to a score of 5, be it Human or Computer....  They shall gain a fabulous prize!'
 let speed = 60;
 
