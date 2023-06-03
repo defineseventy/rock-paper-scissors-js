ceb59d2aba4f61d2cb35eabd58a1d4ad321b360b
defineseventy <jacweizhen6679@gmail.com>
2023-06-03 17:01:19 +0800

    Update index.js
    added some typewriter stuff

--- index.js
+++ index.js
@@ -7,6 +7,22 @@ let playScore = 0;
 let button = document.querySelectorAll(".button");
 const body = document.querySelectorAll("body");
 const main = document.querySelectorAll("main");
-function getComputerChoice(){
-  
+let txt = 'Made using Javascript for a TOP project \n Are you brave enough to challenge the Almighty Computer? \n If you dare to take on this challenge.... The first to reach to a score of 5, be it Human or Computer....  They shall gain a fabulous prize!'
+let speed = 60;
+
+function typeWriter(){
+  if (i < txt.length) {
+    document.getElementById("fade").value += txt.charAt(i);
+    i++;
+    setTimeout(typeWriter, speed);
+  }
 }
+
+/*Set game array here */
+const gameArray = ['Rock', 'Paper', 'Scissorss'];
+function getComputerChoice(){
+  /* Use array and set it to random choice */
+  return gameArray[~~(Math.random()*gameArray.length)]
+}
+
+
