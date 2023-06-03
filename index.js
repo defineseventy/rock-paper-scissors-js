e454cc8c672ac5f11d04b7969d71af57e0a0e1c8
defineseventy <jacweizhen6679@gmail.com>
2023-06-03 16:24:27 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,12 @@
+window.onload = beginningAnimation();
+let compSelect;
+let playSelect;
+let compScore = 0;
+let playScore = 0;
+
+let button = document.querySelectorAll(".button");
+const body = document.querySelectorAll("body");
+const main = document.querySelectorAll("main");
 function getComputerChoice(){
   
 }
