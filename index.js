d25cb6984117302308f09f359b2692223141ceb8
Jacqueline Lim <jacweizhen6679@gmail.com>
2024-03-29 09:29:27 +0800

    Update index.js

--- index.js
+++ index.js
@@ -8,21 +8,17 @@ let button = document.querySelectorAll(".button");
 const body = document.querySelectorAll("body");
 const main = document.querySelectorAll("main");
 let i = 0;
-let txt = 'Made using Javascript for a TOP project \n Are you brave enough to challenge the Almighty Computer? \n If you dare to take on this challenge.... The first to reach to a score of 5, be it Human or Computer....  They shall gain a fabulous prize!'
+let txt = 'Made using Javascript for a TOP project \n Are you brave enough to challenge the Almighty Computer? \n If you dare to take on this challenge....\n The first to reach to a score of 5, be it Human or Computer.... \n They shall gain a fabulous prize!'
 let speed = 60;
 
-function typeWriter(){
-  if (i < txt.length) {
-    document.getElementById("fade").value += txt.charAt(i);
-    i++;
-    setTimeout(typeWriter, speed);
-  }
-}
+typewriterConfig = [txt, true]
+  typewriter();
 
 /*Set game array here */
 const gameArray = ['Rock', 'Paper', 'Scissors'];
 function getComputerChoice(){
   /* Use array and set it to random choice */
+  
   return gameArray[~~(Math.random()*gameArray.length)]
 }
 
